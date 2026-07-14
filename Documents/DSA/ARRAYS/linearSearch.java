06e36a6cf4c280f627b7ff9489571e48070f8cbe
Kundiya <shivanayak20562@gmail.com>
2026-07-14 07:31:37 +0530

    add linear search implementation

---
+++ Documents/DSA/ARRAYS/linearSearch.java
@@ -0,0 +1,21 @@
+/* 
+Example 1:
+Input:
+ arr[] = 1 2 3 4 5, num = 3  
+Output:
+ 2  `
+Explanation:
+ 3 is present at the 2nd index of the array.
+
+ */
+
+ class Solution{
+    public static int LinearSearch(int arr[],int nums)
+    {
+        
+    }
+ }
+
+public class linearSearch {
+    
+}
